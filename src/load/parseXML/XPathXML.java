1745e796c24e87a9484247bd6dca620c9762dd18
Cons Brag <bragkostiy99@gmail.com>
2020-12-16 09:02:10 +0300

    add save to file.txt add input path manual

--- src/load/parseXML/XPathXML.java
+++ src/load/parseXML/XPathXML.java
@@ -1,6 +1,7 @@
 package load.parseXML;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.xml.parsers.DocumentBuilder;
@@ -20,70 +21,52 @@ public class XPathXML {
     public static void main(String[] args) {
         try {
             DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document document = documentBuilder.parse("C:\\Users\\admin\\IdeaProjects\\parseXML\\src\\xml\\example\\record\\testXML.xml");
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите путь к file.xml");
+            String path = in.nextLine();
+            path = path.replace("\\","\\\\");
+            //C:\Users\admin\IdeaProjects\parseXML\src\xml\example\record\testXML.xml
+            Document document = documentBuilder.parse(path);
+            System.out.println("Сохранить в текстовый файл? - \"print yes\"");
+            path = in.nextLine();
+            if("yes".equals(path)){
+                // Создаём поток для сохранения выводимых данных
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                PrintStream ps = new PrintStream(baos);
 
-            //printAll(document);
-            printErrorCor(document);
-//            printCost3(document);
-//            printCost4(document);
-//            printCost5(document);
+                // Сохраняем оригинальный поток вывода
+                PrintStream originalStdOut = System.out;
+
+                // Заменяем поток вывода на наш буфер
+                System.setOut(ps);
+
+                // Вызываем функцию, вывод которой нужно перехватить
+                printErrorCor(document);
+
+                // Восстанавливаем оригинальный поток вывода
+                System.out.flush();
+                System.setOut(originalStdOut);
+
+                // Сохраняем в файл перехваченные данные
+                try (OutputStream fos = new FileOutputStream("ERRORS.txt")) {
+                    baos.writeTo(fos);
+                }
+            }
+            else printErrorCor(document);
 
         } catch (XPathExpressionException | ParserConfigurationException | SAXException | IOException ex) {
             ex.printStackTrace(System.out);
         }
     }
 
-
-    // All
-    private static void printAll(Document document) throws DOMException, XPathExpressionException {
-        System.out.println("Example 1 - Печать всех элементов HTTPSamplerProxy");
-        XPathFactory pathFactory = XPathFactory.newInstance();
-        XPath xpath = pathFactory.newXPath();
-        String s;
-
-        // Пример записи XPath
-        // Подный путь до элемента
-        //XPathExpression expr = xpath.compile("testXML/jmeterTestPlan/TestPlan/stringProp");
-        // Все элементы с таким именем
-        //XPathExpression expr = xpath.compile("//stringProp");
-        // Элементы, вложенные в другой элемент
-        XPathExpression exprName = xpath.compile("//jmeterTestPlan/hashTree/hashTree/hashTree/hashTree/hashTree/HTTPSamplerProxy/attribute::testname");
-        XPathExpression expr = xpath.compile("//jmeterTestPlan/hashTree/hashTree/hashTree/hashTree/hashTree/HTTPSamplerProxy/elementProp/collectionProp/elementProp//stringProp[@name='Argument.value']");
-        XPathExpression exprMethod = xpath.compile("//jmeterTestPlan/hashTree/hashTree/hashTree/hashTree/hashTree/HTTPSamplerProxy//stringProp[@name='HTTPSampler.method']");
-        XPathExpression exprURL = xpath.compile("//jmeterTestPlan/hashTree/hashTree/hashTree/hashTree/hashTree/HTTPSamplerProxy//stringProp[@name='HTTPSampler.path']");
-
-        NodeList nodesName = (NodeList) exprName.evaluate(document, XPathConstants.NODESET);
-        NodeList nodesValue = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
-        NodeList nodesMethod = (NodeList) exprMethod.evaluate(document, XPathConstants.NODESET);
-        NodeList nodesURL = (NodeList) exprURL.evaluate(document, XPathConstants.NODESET);
-
-        for (int i = 0; i < nodesName.getLength(); i++) {
-            Node name = nodesName.item(i);
-            Node n = nodesValue.item(i);
-            Node method = nodesMethod.item(i);
-            Node url = nodesURL.item(i);
-
-            System.out.println("Name:" + name.getTextContent());
-            s = method.getTextContent();
-            if(!s.equals("GET")){
-                System.out.println("Value:" + n.getTextContent());
-            }
-            System.out.println("Method:" + method.getTextContent());
-            System.out.println("URL:" + url.getTextContent() + "\n");
-
-        }
-
-        System.out.println();
-    }
-
     // Errors
     private static void printErrorCor(Document document) throws DOMException, XPathExpressionException {
 
-        System.out.println("Example 1 - Печать всех элементов HTTPSamplerProxy");
+        System.out.println("Вывод всех элементов HTTPSamplerProxy в терминал");
         XPathFactory pathFactory = XPathFactory.newInstance();
         XPath xpath = pathFactory.newXPath();
         String checkValue = "", checkURL, checkMethod;
-        int VALUE_ERROR = 0, URL_ERROR = 0;
+        int VALUE_ERROR = 0, URL_ERROR = 0, k = 0;
 
         // Пример записи XPath
         // Подный путь до элемента
@@ -92,7 +75,6 @@ public class XPathXML {
         //XPathExpression expr = xpath.compile("//stringProp");
         // Элементы, вложенные в другой элемент
         XPathExpression exprName = xpath.compile("//HTTPSamplerProxy/attribute::testname");
-        //[@name='Argument.value']
         XPathExpression expr = xpath.compile("//HTTPSamplerProxy/elementProp/collectionProp/elementProp//stringProp[@name='Argument.value']");
         XPathExpression exprMethod = xpath.compile("//HTTPSamplerProxy//stringProp[@name='HTTPSampler.method']");
         XPathExpression exprURL = xpath.compile("//HTTPSamplerProxy//stringProp[@name='HTTPSampler.path']");
@@ -101,7 +83,7 @@ public class XPathXML {
         NodeList nodesValue = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
         NodeList nodesMethod = (NodeList) exprMethod.evaluate(document, XPathConstants.NODESET);
         NodeList nodesURL = (NodeList) exprURL.evaluate(document, XPathConstants.NODESET);
-        int k = 0;
+
         for (int i = 0; i < nodesName.getLength(); i++) {
 
             Node name = nodesName.item(i);
@@ -118,7 +100,7 @@ public class XPathXML {
                 URL_ERROR++;
             }
             if("POST".equals(checkMethod)){
-                for (int j = k; j < nodesValue.getLength(); k++){
+                for (int j = k; j < nodesValue.getLength();){
                     Node value = nodesValue.item(j);
                     if(value.getTextContent() != null) checkValue = value.getTextContent();
                     Pattern patternValue = Pattern.compile("[^a-zA-Z|^-](\\d{4}\\d+)[^a-zA-Z|-]"); //[^-|a-zA-Z]\d{3}\d+[^\|[a-zA-Z]]
@@ -136,58 +118,11 @@ public class XPathXML {
             System.out.println("Name:" + name.getTextContent());
             System.out.println("Method: " + checkMethod);
             System.out.println("URL: " + checkURL + "\n");
+
         }
             System.out.println("VALUE_ERROR: " + VALUE_ERROR);
             System.out.println("URL_ERROR: " + URL_ERROR);
+
     }
-
-//    // Печать элементов Book у которых значение Cost > 4
-//    private static void printCost3(Document document) throws DOMException, XPathExpressionException {
-//        System.out.println("Example 3 - Печать элементов Book у которых значение Cost > 4");
-//        XPathFactory pathFactory = XPathFactory.newInstance();
-//        XPath xpath = pathFactory.newXPath();
-//        XPathExpression expr = xpath.compile("BookCatalogue/Book[Cost>4]");
-//        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
-//        for (int i = 0; i < nodes.getLength(); i++) {
-//            Node n = nodes.item(i);
-//            System.out.println("Value:" + n.getTextContent());
-//        }
-//        System.out.println();
-//    }
-//
-//    // Печать первого элемента Book
-//    private static void printCost4(Document document) throws DOMException, XPathExpressionException {
-//        System.out.println("Example 4 - Печать первого элемента Book");
-//        XPathFactory pathFactory = XPathFactory.newInstance();
-//        XPath xpath = pathFactory.newXPath();
-//        XPathExpression expr = xpath.compile("BookCatalogue/Book[2]");
-//        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
-//        for (int i = 0; i < nodes.getLength(); i++) {
-//            Node n = nodes.item(i);
-//            System.out.println("Value:" + n.getTextContent());
-//        }
-//        System.out.println();
-//    }
-//
-//    // Печать цены книги у которой Title начинается с Yogasana
-//    // Варианты доступа к относительным узлам:
-//    // ancestor , ancestor-or-self, descendant, descendant-or-self
-//    // following, following-sibling, namespace, preceding, preceding-sibling
-//    private static void printCost5(Document document) throws DOMException, XPathExpressionException {
-//        System.out.println("Example 5 - Печать цены книги у которой Title начинается с 'Yogasana'");
-//        XPathFactory pathFactory = XPathFactory.newInstance();
-//        XPath xpath = pathFactory.newXPath();
-//        XPathExpression expr = xpath.compile("BookCatalogue/Book/Cost"
-//                + "[starts-with(preceding-sibling::Title, 'Yogasana')"
-//                + " or "
-//                + "starts-with(following-sibling::Title, 'Yogasana')]");
-//        NodeList nodes = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
-//        for (int i = 0; i < nodes.getLength(); i++) {
-//            Node n = nodes.item(i);
-//            System.out.println("Value:" + n.getTextContent());
-//        }
-//        System.out.println();
-//    }
-
 }
 
