b3e0fee344b49a3a6ba68eaa3d7a7249c547db92
Cons Brag <bragkostiy99@gmail.com>
2020-12-25 12:15:53 +0300

    add case Errors, Copy Body and Print All

--- src/load/parseXML/XPathXML.java
+++ src/load/parseXML/XPathXML.java
@@ -26,35 +26,114 @@ public class XPathXML {
             String path = in.nextLine();
             path = path.replace("\\","\\\\");
 
-            //C:\Users\admin\IdeaProjects\parseXML\src\xml\example\record\testXML.xml
-            Document document = documentBuilder.parse(new File(path));
-            System.out.println("Save to text file? - print \"yes\"");
-            path = in.nextLine();
+            Document document = documentBuilder.parse(new File(path)); //Документ
+            String search = null; // Строка поиска
+            int seqNumber = 0;
+            System.out.println("Input number case: \n  case 1 - Errors\n  case 2 - Copy Body\n  case 3 - Print All");
+            seqNumber = in.nextInt();;
+            switch (seqNumber) {
+                //Errors
+                case (1):
+                {
+                    System.out.println("Save to text file? - print \"yes\"");
+                    path = in.nextLine();
 
-            if("yes".equals(path)){
-                // Создаём поток для сохранения выводимых данных
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                PrintStream ps = new PrintStream(baos);
+                    if("yes".equals(path)){
+                        // Создаём поток для сохранения выводимых данных
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        PrintStream ps = new PrintStream(baos);
 
-                // Сохраняем оригинальный поток вывода
-                PrintStream originalStdOut = System.out;
+                        // Сохраняем оригинальный поток вывода
+                        PrintStream originalStdOut = System.out;
 
-                // Заменяем поток вывода на наш буфер
-                System.setOut(ps);
+                        // Заменяем поток вывода на наш буфер
+                        System.setOut(ps);
 
-                // Вызываем функцию, вывод которой нужно перехватить
-                printError(document);
+                        // Вызываем функцию, вывод которой нужно перехватить
+                        printError(document);
 
-                // Восстанавливаем оригинальный поток вывода
-                System.out.flush();
-                System.setOut(originalStdOut);
+                        // Восстанавливаем оригинальный поток вывода
+                        System.out.flush();
+                        System.setOut(originalStdOut);
 
-                // Сохраняем в файл перехваченные данные
-                try (OutputStream fos = new FileOutputStream("ERRORS.txt")) {
-                    baos.writeTo(fos);
+                        // Сохраняем в файл перехваченные данные
+                        try (OutputStream fos = new FileOutputStream("ERRORS.txt")) {
+                            baos.writeTo(fos);
+                        }
+                    }
+                    else printError(document);
                 }
+                    break;
+
+                //Copy Body
+                case (2):
+                {
+                    System.out.println("Input object name");
+                    search = in.next();
+
+                    System.out.println("Save to text file? - print \"yes\"");
+                    path = in.next();
+
+                    if("yes".equals(path)){
+                        // Создаём поток для сохранения выводимых данных
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        PrintStream ps = new PrintStream(baos);
+
+                        // Сохраняем оригинальный поток вывода
+                        PrintStream originalStdOut = System.out;
+
+                        // Заменяем поток вывода на наш буфер
+                        System.setOut(ps);
+
+                        // Вызываем функцию, вывод которой нужно перехватить
+                        copyBodyDataHTTPRequest(document, search);
+
+                        // Восстанавливаем оригинальный поток вывода
+                        System.out.flush();
+                        System.setOut(originalStdOut);
+
+                        // Сохраняем в файл перехваченные данные
+                        try (OutputStream fos = new FileOutputStream("ERRORS.txt")) {
+                            baos.writeTo(fos);
+                        }
+                    }
+                    else copyBodyDataHTTPRequest(document,search);
+                }
+                    break;
+
+                //Print All
+                default:
+                {
+                    System.out.println("Save to text file? - print \"yes\"");
+                    path = in.nextLine();
+
+                    if("yes".equals(path)){
+                        // Создаём поток для сохранения выводимых данных
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        PrintStream ps = new PrintStream(baos);
+
+                        // Сохраняем оригинальный поток вывода
+                        PrintStream originalStdOut = System.out;
+
+                        // Заменяем поток вывода на наш буфер
+                        System.setOut(ps);
+
+                        // Вызываем функцию, вывод которой нужно перехватить
+                        printAll(document);
+
+                        // Восстанавливаем оригинальный поток вывода
+                        System.out.flush();
+                        System.setOut(originalStdOut);
+
+                        // Сохраняем в файл перехваченные данные
+                        try (OutputStream fos = new FileOutputStream("ERRORS.txt")) {
+                            baos.writeTo(fos);
+                        }
+                    }
+                    else printAll(document);
+                }
+                    break;
             }
-            else printError(document);
 
         } catch (XPathExpressionException | ParserConfigurationException | SAXException | IOException ex) {
             ex.printStackTrace(System.out);
@@ -64,7 +143,7 @@ public class XPathXML {
     // Errors
     private static void printError(Document document) throws DOMException, XPathExpressionException {
 
-        System.out.println("Outputting all HTTPSamplerProxy elements to the console");
+        System.out.println("Outputting errors don't correlated elements");
         XPathFactory pathFactory = XPathFactory.newInstance();
         XPath xpath = pathFactory.newXPath();
         String checkValueFull = "", checkURL, checkMethod, checkValue, checkName;
@@ -146,6 +225,119 @@ public class XPathXML {
         }
         System.out.println("VALUE_ERROR: " + VALUE_ERROR);
         System.out.println("URL_ERROR: " + URL_ERROR);
+    }
+
+    // Copy Body Data
+    private static void copyBodyDataHTTPRequest(Document document, String search) throws DOMException, XPathExpressionException {
+
+        System.out.println("Outputting of found items");
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+        String checkValueFull, checkURL, checkMethod, checkName;
+        String checkValueFind = null;
+
+        XPathExpression expr = xpath.compile("//HTTPSamplerProxy");
+        XPathExpression exprName = xpath.compile("//HTTPSamplerProxy/attribute::testname");
+        XPathExpression exprMethod = xpath.compile("//HTTPSamplerProxy//stringProp[@name='HTTPSampler.method']");
+        XPathExpression exprURL = xpath.compile("//HTTPSamplerProxy//stringProp[@name='HTTPSampler.path']");
+
+        NodeList nodesValue = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        NodeList nodesName = (NodeList) exprName.evaluate(document, XPathConstants.NODESET);
+        NodeList nodesMethod = (NodeList) exprMethod.evaluate(document, XPathConstants.NODESET);
+        NodeList nodesURL = (NodeList) exprURL.evaluate(document, XPathConstants.NODESET);
+
+        for (int i = 0; i < nodesValue.getLength(); i++) {
+
+            Node value = nodesValue.item(i);
+            Node name = nodesName.item(i);
+            Node method = nodesMethod.item(i);
+            Node url = nodesURL.item(i);
+
+            checkName = name.getTextContent();
+            checkMethod = method.getTextContent();
+            checkURL = url.getTextContent();
+            checkValueFull = value.getTextContent();
+
+            if(value.getTextContent() != null) checkValueFull = value.getTextContent();
+            Pattern patternValueFind = Pattern.compile("([^$])[{](.+)([}])\\n");
+            Matcher matcherValueFind = patternValueFind.matcher(checkValueFull);
+
+            while (matcherValueFind.find()) {
+                checkValueFind = checkValueFull.substring(matcherValueFind.start(), matcherValueFind.end());
+            }
+
+            if (checkValueFind != null && checkValueFind.length() > 0) {
+                checkValueFind = checkValueFind.substring(0, checkValueFind.length() - 1);
+            }
+
+            if(search.equals(checkName)){
+                System.out.println("Name: " + checkName);
+                System.out.println("Value: " + checkValueFind);
+                System.out.println("Method: " + checkMethod);
+                System.out.println("URL: " + checkURL + "\n");
+            }
+
+            checkValueFind = "";
+            checkName = "";
+            checkMethod = "";
+            checkURL = "";
+
+        }
+    }
+
+    // Print All
+    private static void printAll(Document document) throws DOMException, XPathExpressionException {
+
+        System.out.println("Outputting all elements");
+        XPathFactory pathFactory = XPathFactory.newInstance();
+        XPath xpath = pathFactory.newXPath();
+        String checkValueFull = "", checkURL, checkMethod, checkValue, checkName;
+        String checkValueFind = null;
+
+        XPathExpression expr = xpath.compile("//HTTPSamplerProxy");
+        XPathExpression exprName = xpath.compile("//HTTPSamplerProxy/attribute::testname");
+        XPathExpression exprMethod = xpath.compile("//HTTPSamplerProxy//stringProp[@name='HTTPSampler.method']");
+        XPathExpression exprURL = xpath.compile("//HTTPSamplerProxy//stringProp[@name='HTTPSampler.path']");
+
+        NodeList nodesValue = (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+        NodeList nodesName = (NodeList) exprName.evaluate(document, XPathConstants.NODESET);
+        NodeList nodesMethod = (NodeList) exprMethod.evaluate(document, XPathConstants.NODESET);
+        NodeList nodesURL = (NodeList) exprURL.evaluate(document, XPathConstants.NODESET);
+
+        for (int i = 0; i < nodesValue.getLength(); i++) {
+
+            Node value = nodesValue.item(i);
+            Node name = nodesName.item(i);
+            Node method = nodesMethod.item(i);
+            Node url = nodesURL.item(i);
+
+            checkName = name.getTextContent();
+            checkMethod = method.getTextContent();
+            checkURL = url.getTextContent();
+            checkValueFull = value.getTextContent();
+
+            if(value.getTextContent() != null) checkValueFull = value.getTextContent();
+            Pattern patternValueFind = Pattern.compile("([^$])[{](.+)([}])\\n");
+            Matcher matcherValueFind = patternValueFind.matcher(checkValueFull);
+
+            while (matcherValueFind.find()) {
+                checkValueFind = checkValueFull.substring(matcherValueFind.start(), matcherValueFind.end());
+            }
+
+            if (checkValueFind != null && checkValueFind.length() > 0) {
+                checkValueFind = checkValueFind.substring(0, checkValueFind.length() - 1);
+            }
+
+            System.out.println("Name: " + checkName);
+            System.out.println("Value: " + checkValueFind);
+            System.out.println("Method: " + checkMethod);
+            System.out.println("URL: " + checkURL + "\n");
+
+            checkValueFind = "";
+            checkName = "";
+            checkMethod = "";
+            checkURL = "";
+        }
     }
 }
 
